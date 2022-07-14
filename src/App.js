2c70e3060d06e815bae98be047a555f3bfc23699
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-14 15:21:01 -0500

    began styling for mobile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
 	return (
 		<div className='App'>
-			<div className='w-full min-h-screen bg-cream_bg'></div>
+			<div className='w-full min-h-screen flex items-center justify-center bg-cream_bg'>
+				{/* card */}
+				<div className='w-343 h-534 flex flex-col justify-between'>
+					{/* balance */}
+					<div className='w-343 h-97 bg-orange_main rounded-10'></div>
+					{/* chart */}
+					<div className='w-343 h-421 bg-card_white rounded-10'></div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
+			borderRadius: {
+				10: "10px",
+			},
 			colors: {
 				cream_bg: "#F8E9DD",
 				orange_main: "#EC755D",
@@ -11,6 +14,14 @@ module.exports = {
 				card_white: "#FFFBF6",
 				black: "#382314",
 				medium_brown: "#92857A",
+			},
+			height: {
+				97: "97px",
+				421: "421px",
+				534: "534px",
+			},
+			width: {
+				343: "343px",
 			},
 		},
 	},
