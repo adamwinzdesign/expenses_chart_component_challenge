05eb265d92d30c134f382a899fb4eafaa53cedb2
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-14 14:50:24 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				cream_bg: "#F8E9DD",
+				orange_main: "#EC755D",
+				orange_light: "FF9B86",
+				cyan: "#76B5BC",
+				card_white: "#FFFBF6",
+				black: "#382314",
+				medium_brown: "#92857A",
+			},
+		},
+	},
+	plugins: [],
+};
