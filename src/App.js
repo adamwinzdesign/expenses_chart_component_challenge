0b2bc9458b893786a174a01eb899c316c056687d
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-14 17:24:00 -0500

    updated balance section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
 				{/* card */}
 				<div className='w-343 h-534 flex flex-col justify-between'>
 					{/* balance */}
-					<div className='w-343 h-97 bg-orange_main rounded-10'></div>
+					<div className='w-343 h-97 p-38 bg-orange_main rounded-10 flex items-center justify-between'>
+						<div>
+							<p className='text-white font-sans text-font15 leading-5'>My balance</p>
+							<h2 className='text-white font-sans font-bold text-font24 leading-31'>$921.48</h2>
+						</div>
+
+						<div className='w-72 h-12 bg-logo' />
+					</div>
 					{/* chart */}
 					<div className='w-343 h-421 bg-card_white rounded-10'></div>
 				</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
+			backgroundImage: {
+				logo: "url(./images/logo.svg)",
+			},
 			borderRadius: {
 				10: "10px",
 			},
@@ -15,12 +18,26 @@ module.exports = {
 				black: "#382314",
 				medium_brown: "#92857A",
 			},
+			fontFamily: {
+				sans: ["DM Sans", "sans-serif"],
+			},
+			fontSize: {
+				font15: "15px",
+				font24: "24px",
+			},
 			height: {
 				97: "97px",
 				421: "421px",
 				534: "534px",
 			},
+			lineHeight: {
+				31: "31px",
+			},
+			spacing: {
+				38: "38px",
+			},
 			width: {
+				72: "72px",
 				343: "343px",
 			},
 		},
