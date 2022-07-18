db9944c139c2e0c3cb9d46f6338f6cc988d8ff7d
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-18 11:09:33 -0500

    updating ChartRef

--- src/components/ChartRef.jsx
+++ src/components/ChartRef.jsx
@@ -1,12 +1,44 @@
 import { data as defaultData } from "../data.jsx";
+import { useRef } from "react";
+import Chartjs from "chart.js";
 
 const ChartRef = () => {
+	const chartRef = useRef(null);
+
 	const labels = defaultData.map((data) => data.day);
 	const amount = defaultData.map((data) => data.amount);
 
-	console.log({ labels, amount });
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				data: amount,
+				backgroundColor: "#f00",
+				hoverBackgroundColor: "purple",
+				borderSkipped: false,
+				borderRadius: 5,
+				datalabels: {
+					display: true,
+				},
+			},
+		],
+	};
 
-	return <div className='relative h-201 mt-5 mb-6'>ChartRef!</div>;
+	const config = {
+		type: "bar",
+		data,
+		options: {
+			onHover: (event, chartElement) => console.log({ event, chartElement }),
+		},
+		scales: {},
+		plugins: {},
+	};
+
+	return (
+		<div className='relative h-201 mt-5 mb-6'>
+			<canvas ref={chartRef} />
+		</div>
+	);
 };
 
 export default ChartRef;
