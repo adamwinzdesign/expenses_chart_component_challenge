6c1a11c0f9a183af00b4fe4759e503d574c61a3e
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-18 14:28:01 -0500

    updating ChartRef

--- src/components/ChartRef.jsx
+++ src/components/ChartRef.jsx
@@ -1,8 +1,46 @@
 import { data as defaultData } from "../data.jsx";
 import { useRef, useEffect } from "react";
-import { Chart } from "chart.js";
+// import { Chart, BarElement } from "chart.js/auto";
+import Chart from "chart.js/auto";
 
-const chartConfig = {};
+// Chart.register(BarElement);
+
+const labels = defaultData.map((data) => data.day);
+const amount = defaultData.map((data) => data.amount);
+
+const data = {
+	labels: labels,
+	datasets: [
+		{
+			data: amount,
+			backgroundColor: function (context) {
+				const today = new Date().getDay() - 1;
+				const index = context.dataIndex;
+				return index === today ? "#76B5BC" : "#EC755D";
+			},
+			hoverBackgroundColor: function (context) {
+				const today = new Date().getDay() - 1;
+				const index = context.dataIndex;
+				return index === today ? "#B4E0E5" : "#FF9B86";
+			},
+			borderSkipped: false,
+			borderRadius: 5,
+			datalabels: {
+				display: true,
+			},
+		},
+	],
+};
+
+const chartConfig = {
+	type: "bar",
+	data,
+	options: {
+		onHover: (event, chartElement) => {
+			event.native.target.style.cursor = chartElement[0] ? "pointer" : "default";
+		},
+	},
+};
 
 const ChartRef = () => {
 	const chartRef = useRef(null);
@@ -15,7 +53,7 @@ const ChartRef = () => {
 	}, [chartRef]);
 
 	return (
-		<div className='relative h-201 mt-5 mb-6'>
+		<div className='border-2 relative h-201 mt-5 mb-6 flex items-end'>
 			<canvas ref={chartRef} id='chartRef' />
 		</div>
 	);
